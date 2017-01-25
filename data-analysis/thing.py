6c63e616800abcf28d3b76574dda68fca3d63094
Ben <nagy.benb@gmail.com>
2017-01-25 22:20:59 +0000

    Working through Quiz:Getting Data from First Week

--- data-analysis/thing.py
+++ data-analysis/thing.py
@@ -14,6 +14,18 @@ def get_unique_students(dataset):
         unique_students.add(entry['account_key'])
     return unique_students
 
+def parse_maybe_int(value):
+    if value == '':
+        return None
+    else:
+        return int(value)
+
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(value, '%Y-%m-%d')
+
 enrollments = read_csv('enrollments.csv')
 daily_engagement = read_csv('daily_engagement.csv')
 project_submissions = read_csv('project_submissions.csv')
@@ -35,19 +47,39 @@ del enrollments_no_test
 
 # Fix data types
 for enrollment in enrollments:
-    enrollment['cancel_date'] = None if enrollment['cancel_date'] == '' else dt.strptime(enrollment['cancel_date'], '%Y-%m-%d')
-    enrollment['days_to_cancel'] = None if enrollment['days_to_cancel'] == '' else int(enrollment['days_to_cancel'])
+    enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
+    enrollment['days_to_cancel'] = parse_maybe_int(enrollment['days_to_cancel'])
     enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
     enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
-    enrollment['join_date'] = None if enrollment['join_date'] == '' else dt.strptime(enrollment['join_date'], '%Y-%m-%d')
+    enrollment['join_date'] = parse_date(enrollment['join_date'])
 
-print enrollments[0]
+for engagement in daily_engagement:
+    engagement['account_key'] = parse_maybe_int(engagement['account_key'])
+    engagement['utc_date'] = parse_date(engagement['utc_date'])
+    engagement['num_courses_visited'] = parse_maybe_int(engagement['num_courses_visited'])
+    engagement['total_minutes_visited'] = parse_maybe_int(engagement['total_minutes_visited'])
+    engagement['lessons_completed'] = parse_maybe_int(engagement['lessons_completed'])
+    engagement['projects_completed'] = parse_maybe_int(engagement['projects_completed'])
+
+for submission in project_submissions:
+    submission['creation_date'] = parse_date(submission['creation_date'])
+    submission['completion_date'] = parse_date(submission['completion_date'])
+    submission['assigned_rating'] = parse_maybe_int(submission['assigned_rating'])
+    submission['account_key'] = parse_maybe_int(submission['account_key'])
+    submission['lesson_key'] = parse_maybe_int(submission['lesson_key'])
+    # submission['processing_state'] is a string
 
 # Quiz : Refining the question
 paid_students = dict()
 
+# Find paid students
 for enrollment in enrollments:
     if enrollment['days_to_cancel'] == None or enrollment['days_to_cancel'] > 7:
         paid_students[enrollment['account_key']] = enrollment['join_date']
 
 print len(paid_students)
+
+# Find paid students' engagements for the first week
+for engagement in daily_engagement:
+    if engagement['account_key'] in paid_students \
+    and engagement['utc-date'] <
